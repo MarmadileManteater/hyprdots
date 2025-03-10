224e8c275040402a05d4735d3bbbcf4fc96e033c
Emma <MarmadileManteater@proton.me>
2025-03-09 21:15:44 -0400

    Add an `attach` flag to run in tmux

--- hypr/.config/hypr/scripts/run-in-tmux.sh
+++ hypr/.config/hypr/scripts/run-in-tmux.sh
@@ -1,7 +1,18 @@
 
+
 session_name=$1
 command=$2
+flags=$3
 
 tmux new-session -d -s $session_name "/bin/bash"
 
 tmux send-keys -t$session_name "$command" Enter
+
+
+if [[ "$flags" == "-"* ]]
+then
+    if [[ "$flags" == *"a"* ]]
+    then
+        tmux attach-session -t$session_name
+    fi
+fi
