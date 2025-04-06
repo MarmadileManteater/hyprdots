51609461b19fbe3e7b3acd34ce44ad1d752e550c
Emma <MarmadileManteater@proton.me>
2025-04-06 12:25:18 -0400

    Fix issues causing slowdown with album art script
    - reduce number of calls to image magick to only when necessary

--- waybar/.config/waybar/modules/media-notif/album_art.sh
+++ waybar/.config/waybar/modules/media-notif/album_art.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 album_art=$(playerctl metadata mpris:artUrl)
-if [[ -z $album_art ]] 
+if [[ "$album_art" != "" ]]
 then
-   rm /tmp/cover.jpeg
+   if [[ "$album_art" != "$(cat /tmp/recent_cover.txt)" ]]
+   then
+      echo "$album_art" >> /tmp/recent_cover.txt
+      local_uri="${album_art:7}"
+
+      magick $local_uri /tmp/cover.jpeg&
+   fi
+   echo "/tmp/cover.jpeg"
+else
+   if [[ -f "/tmp/cover.jpeg" ]]
+   then
+      rm /tmp/recent_cover.txt
+      rm /tmp/cover.jpeg
+   fi
 fi
-local_uri="${album_art:7}"
-magick $local_uri /tmp/cover.jpeg&
-echo "/tmp/cover.jpeg"
