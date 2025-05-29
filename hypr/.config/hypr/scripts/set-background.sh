417bfce485172124bf4573d7f6c2438abd7a893a
Emma <MarmadileManteater@proton.me>
2025-05-29 17:40:32 -0400

    Add accent color to power menu based on background image

--- hypr/.config/hypr/scripts/set-background.sh
+++ hypr/.config/hypr/scripts/set-background.sh
@@ -4,3 +4,7 @@ file=$1
 cp "$1" $HOME/.config/hypr/background
 
 swww img $HOME/.config/hypr/background
+
+most_common_color=$(magick $HOME/.config/hypr/background -colors 1 -unique-colors txt: | tail -n1 | grep -o '#\(.*\) ')
+
+sed -i "s@/\*\* accent \*/.*/\*\* /accent \*/@/** accent */ $most_common_color /** /accent */@g" ~/.config/waybar/modules/wlogout/power-menu.css &> /dev/null
