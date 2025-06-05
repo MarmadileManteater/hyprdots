94dc319b3c3fb9f83eae28ec38cb218cbb34f17e
Emma <MarmadileManteater@proton.me>
2025-06-04 23:23:17 -0400

    Adjust debounce timeout for waybar show/hide

--- hypr/.config/hypr/scripts/show-hide-waybar.sh
+++ hypr/.config/hypr/scripts/show-hide-waybar.sh
@@ -6,9 +6,9 @@ fi
 
 current_timestamp=$(date +%s%3N)
 last_timestamp=$(cat /tmp/last-waybar-show-hide.txt)
-echo $(echo "$current_timestamp - $last_timestamp" | bc)
+
 # waybar freezes if you send this signal too much
-if [ $(echo "$current_timestamp - $last_timestamp" | bc) -gt 1000 ]
+if [ $(echo "$current_timestamp - $last_timestamp" | bc) -gt 1500 ]
 then
   pkill -SIGUSR1 waybar
   echo $current_timestamp &> /tmp/last-waybar-show-hide.txt
