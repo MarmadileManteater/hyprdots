ef425186ba8a1157385cbb0607066adad0232a8e
Emma <MarmadileManteater@proton.me>
2026-01-02 12:33:21 -0500

    Improve flip between alt and super between my keyboards

--- hypr/.config/hypr/scripts/flip-main-mod.sh
+++ hypr/.config/hypr/scripts/flip-main-mod.sh
@@ -3,17 +3,27 @@
 mainMods=("SUPER" "ALT_L")
 
 function get_current_main_mod() {
-  local value=$(cat ~/.hyprdots/hypr/.config/hypr/hyprland.conf | grep '$mainMod = \(.*\)#' -o)
-  echo ${value:10:-1}
+  local kb_options=$(cat ~/.hyprdots/hypr/.config/hypr/hyprland.conf | grep 'kb_options = \(.*\)#' -o)
+  local value="${kb_options:13:-1}"
+  if [[ "$value" == "altwin:swap_alt_win" ]]
+  then
+    echo "ALT_L"
+  else
+    echo "SUPER"
+  fi
 }
 
 function set_current_main_mod() {
   local set_to=$1
-  if [ "$set_to" == "" ]
+  if [ "$set_to" == "SUPER" ]
   then
-    set_to="SUPER"
+    set_to=""
   fi
-  sed 's@$mainMod = .*#@$mainMod = '$1'#@g' ~/.hyprdots/hypr/.config/hypr/hyprland.conf -i
+  if [ "$set_to" == "ALT_L" ]
+  then
+    set_to="altwin:swap_alt_win"
+  fi
+  sed 's@kb_options = .*#@kb_options = '$set_to'#@g' ~/.hyprdots/hypr/.config/hypr/hyprland.conf -i
 }
 
 mainMod=$(get_current_main_mod)
