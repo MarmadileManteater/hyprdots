51e5dc2a1c2ce4e3559b6d21fb47a020b3bafee5
Emma <MarmadileManteater@proton.me>
2026-01-04 10:44:21 -0500

    add `--no-hash` arg to `get-accent`

--- hypr/.config/hypr/scripts/get-accent-color.sh
+++ hypr/.config/hypr/scripts/get-accent-color.sh
@@ -1,3 +1,10 @@
 #/bin/bash
 
-cat $HOME/.config/hypr/accent.txt
+hex=$(cat $HOME/.config/hypr/accent.txt)
+
+if [ "$(echo "$@" | grep '\--no-hash' -o)" != "" ]
+then
+  echo ${hex:1:6}
+else
+  echo $hex
+fi
