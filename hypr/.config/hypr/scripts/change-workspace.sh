2374684e980b22f6264135c46cf4f6a716e99ad6
Emma <MarmadileManteater@proton.me>
2026-01-07 12:01:44 -0500

    Add ability to scroll on workspaces indicator
    (to change workspaces)

--- hypr/.config/hypr/scripts/change-workspace.sh
+++ hypr/.config/hypr/scripts/change-workspace.sh
@@ -1,6 +1,7 @@
 
 amount=-10
 command="movetoworkspace"
+wrap=0
 
 if [ "$1" != "" ]
 then
@@ -12,9 +13,34 @@ then
     command="workspace"
 fi
 
+if [ "$3" == "wrap" ]
+then
+    wrap=1
+fi
+
 active_workspace_id=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .activeWorkspace | .id')
-echo "$active_workspace_id $amount"
+
 workspace_id=$(echo "$active_workspace_id $amount" | bc)
+
+if [ $wrap -eq 1 ]
+then
+
+    if [[ $workspace_id == -* ]]
+    then
+        monitor=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .id')
+        target_workspace=$(hyprctl workspaces -j | jq ".[] | select(.monitorID == $monitor) | .id" | tail -1)
+        amount="$target_workspace"
+        workspace_id="$target_workspace"S
+    fi
+
+    exists=$(hyprctl workspaces -j | jq ".[] | select(.id == $workspace_id) | .id")
+    if [ "$exists" == "" ]
+    then
+        offset_amount="$(echo "(($active_workspace_id / 10) * 10)" | bc)"
+        workspace_id="$(echo "$active_workspace_id - $offset_amount" | bc)"
+    fi
+fi
+
 if [[ $workspace_id == -* ]]
 then
     echo "Can't decrement"
@@ -23,5 +49,5 @@ else
     then
         command="workspace"
     fi
-    hyprctl dispatch $command $amount
+    hyprctl dispatch $command $workspace_id
 fi
